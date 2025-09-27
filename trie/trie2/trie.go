670c52f9a0da25381c9c8e214fbb629466354753
aryan1910 <aryansongira042@gmail.com>
2025-09-27 15:24:50 +0530

    Add trie ii

---
+++ trie/trie2/trie.go
@@ -0,0 +1,84 @@
+package trie2
+
+type Node struct {
+	links       map[rune]*Node
+	countEnd    int
+	countPrefix int
+}
+
+func NewNode() *Node {
+	return &Node{
+		links:       make(map[rune]*Node),
+		countEnd:    0,
+		countPrefix: 0,
+	}
+}
+
+type Trie struct {
+	root *Node
+}
+
+func Constructor() Trie {
+	return Trie{
+		root: NewNode(),
+	}
+}
+
+func (t *Trie) Insert(word string) {
+	temp := t.root
+
+	for _, c := range word {
+		_, ok := temp.links[c]
+		if !ok {
+			temp.links[c] = NewNode()
+		}
+		temp = temp.links[c]
+		temp.countPrefix++
+	}
+	temp.countEnd++
+}
+
+func (t *Trie) countWordsEqualTo(word string) int {
+	temp := t.root
+	for _, c := range word {
+		_, ok := temp.links[c]
+		if !ok {
+			return 0
+		}
+		temp = temp.links[c]
+	}
+	return temp.countEnd
+}
+
+func (t *Trie) countWordsStartingWith(prefix string) int {
+	temp := t.root
+	for _, c := range prefix {
+		_, ok := temp.links[c]
+		if !ok {
+			return 0
+		}
+		temp = temp.links[c]
+	}
+	return temp.countPrefix
+}
+
+func (t *Trie) erase(prefix string) {
+	temp := t.root
+	for _, c := range prefix {
+		_, ok := temp.links[c]
+		if !ok {
+			return
+		}
+		temp = temp.links[c]
+		temp.countPrefix--
+	}
+	temp.countEnd--
+}
+
+/**
+ * Your Trie object will be instantiated and called as such:
+ * obj := Constructor();
+ * obj.Insert(word);
+ * param_2 := obj.Search(word);
+ * param_3 := obj.StartsWith(prefix);
+ */
