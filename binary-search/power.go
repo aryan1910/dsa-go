fe2682a327d8ca1a4640b49ce5caf6f11a91008e
aryan1910 <aryansongira042@gmail.com>
2025-10-19 17:57:49 +0530

    fix indentation

--- binary-search/power.go
+++ binary-search/power.go
@@ -3,33 +3,33 @@
 package binary_search
 
 func AbsInt(x int) int {
-    if x < 0 {
-        return -x
-    }
-    return x
+	if x < 0 {
+		return -x
+	}
+	return x
 }
-func isNegative(n int) bool{
-    if n<0{
-        return true
-    }
-    return false
+func isNegative(n int) bool {
+	if n < 0 {
+		return true
+	}
+	return false
 }
 func myPow(x float64, n int) float64 {
-    neg := isNegative(n)
-    n = AbsInt(n)
-    ans := 1.0
-    mul := x
-    for n > 0 {
-        if n%2==1{
-            ans = ans * mul;
-        }
-        if n>1{
-        mul = mul*mul
-        }
-        n = n/2
-    }
-    if neg {
-        return 1.0/ans;
-    }
-    return ans;
+	neg := isNegative(n)
+	n = AbsInt(n)
+	ans := 1.0
+	mul := x
+	for n > 0 {
+		if n%2 == 1 {
+			ans = ans * mul
+		}
+		if n > 1 {
+			mul = mul * mul
+		}
+		n = n / 2
+	}
+	if neg {
+		return 1.0 / ans
+	}
+	return ans
 }
